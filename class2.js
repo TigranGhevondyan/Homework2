1bf8832a3fd3cb146b8707f63580d482696fdb50
TigranGhevondyan <60707688+TigranGhevondyan@users.noreply.github.com>
2020-02-10 23:06:38 +0400

    Update class2.js

--- class2.js
+++ class2.js
@@ -22,3 +22,39 @@ return'+';
 console.log(myfunc(-14,5,0))
 console.log(myfunc(-8,9,-6))
 console.log(myfunc(4,19,-2))
+
+
+var myfunc=function(d,num){
+num=String(num)
+for(let i=0; i<num.length; i++){
+if(num[i]==d){
+return 'Yes';
+}
+}
+return 'No';
+}
+console.log(myfunc(5,2463))
+console.log(myfunc(4,6))
+console.log(myfunc(8,45689))
+
+var sort=function(a,b,c){
+let arr=[a,b,c]
+
+return arr.sort(function(a,b){return a - b});
+}
+
+console.log(sort(45,26,78));
+console.log(sort(-23,-456,0));
+
+
+var sort=function(st){
+st=String(st);
+let arr=st.split('').sort(function(a,b){return a - b})
+
+mn=Number(arr[0])
+mx=Number(arr[arr.length-1])
+
+return mx-mn;
+}
+
+console.log(sort(4593653));
